23aeed0b65261c8f72d811cf2b9321b227911801
Luis Torres <luisgabrieltorresvidal@gmail.com>
2023-06-22 18:53:38 -0500

    modificacion id lecturas

--- app/Models/lecturas/lecturas.php
+++ app/Models/lecturas/lecturas.php
@@ -2,18 +2,24 @@
 
 namespace App\Models\lecturas;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class lecturas extends Model
 {
     use HasFactory;
-
+    use HasUuids;
     protected $fillable = [
+
         'study_id',
         'medico_id',
         'estudio',
         'informe',
         'fechaestudio'
+    ];
+
+    protected $casts = [
+        'id' => 'string'
     ];
 }
